f96f9430735b8022726064110212ffd42b0ec853
afonsobalancho <afonsojosebalancho@gmail.com>
2023-03-09 16:58:05 +0000

    fourth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_151506) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_153240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_151506) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "household"
+    t.string "food_preferences"
+    t.string "allergies"
+    t.string "intolerances"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
