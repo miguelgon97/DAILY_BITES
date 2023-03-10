fa18979e5129f10d420a55be24693c243b608c7a
joaoborges <joaoborgesbiz@gmail.com>
2023-03-10 14:39:16 +0000

    made logic with the api to work with the ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_07_153240) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,17 +74,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_153240) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.string "household"
-    t.string "food_preferences"
-    t.string "allergies"
-    t.string "intolerances"
-
     t.string "first_name"
     t.string "last_name"
     t.integer "phone_number"
     t.string "user_name"
-
+    t.string "household"
+    t.string "food_preferences"
+    t.string "allergies"
+    t.string "intolerances"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,48 +7,80 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts" destoying all ingredients"
+# puts" destoying all ingredients"
 
-Ingredient.destroy_all
+# Ingredient.destroy_all
 
-puts "creating some ingredients"
-ingredients = []
- 100.times do
-    ingredients << Ingredient.create(name: Faker::Food.ingredient )
-  end
+# puts "creating some ingredients"
+# ingredients = []
+# 100.times do
+#   ingredients << Ingredient.create(name: Faker::Food.ingredient )
+# end
 
-puts "done"
+# puts "done"
 
-puts "destroying all users"
+# puts "destroying all users"
 
-User.destroy_all
+# User.destroy_all
 
-puts "creating users"
+# puts "creating users"
 
-5.times do
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 123456, user_name: Faker::Internet.username)
-end
+# 5.times do
+#   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 123456, user_name: Faker::Internet.username)
+# end
 
 require "json"
 require "rest-client"
 require 'byebug'
 
-Recipe.destroy_all
+# puts "deleting all recipes"
+# Recipe.destroy_all
+# RecipeIngredient.destroy_all
+# puts "creating recipes"
 
+# 10.times do
+#   recipe = Recipe.create(name: Faker::Food.dish, prep_time: [1..60].sample, description: Faker::Food.description, calories: [100..1000].sample, photo_url: Faker::LoremFlickr.image(size: "50x60", search_terms: ['food']))
+#   10.times do
+#     RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: ingredients.sample.id)
+#   end
+# end
 # => repos is an `Array` of `Hashes`.
 response1 = RestClient.get "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number2&apiKey=#{ENV['SPOON_API_KEY']}"
 repos = JSON.parse(response1)
 
-p repos
+
 repos.each do |repo|
   response2 = RestClient.get "https://api.spoonacular.com/recipes/#{repo['id']}/information?includeNutrition=false&apiKey=#{ENV['SPOON_API_KEY']}"
   recipe_info = JSON.parse(response2)
 
-  Recipe.create(
-    name: repo["title"],
-    rating: repo["spoonacularScore"],
-    photo_url: repo["image"],
-    description: recipe_info["instructions"],
-    prep_time: recipe_info["preparationMinutes"]
-  )
+
+  recipe_info["analyzedInstructions"].each do |el|
+    el["steps"].each do |e|
+      e["ingredients"].each do |element|
+        unless unique_ingredient = Ingredient.find_by(name: element["name"])
+          unique_ingredient = Ingredient.create(name: element["name"])
+        end
+
+        unless unique_recipe = Recipe.find_by(name: repo["title"])
+          unique_recipe = Recipe.create(name: repo["title"], rating: repo["spoonacularScore"], photo_url: repo["image"], description: recipe_info["instructions"], prep_time: recipe_info["preparationMinutes"])
+        end
+
+        unless RecipeIngredient.find_by(recipe: unique_recipe, ingredient: unique_ingredient)
+          RecipeIngredient.create(recipe: unique_recipe, ingredient: unique_ingredient)
+        end
+
+      end
+    end
+  end
 end
+
+puts "Created #{Recipe.count}recipes----#{Ingredient.count}ingredients"
+
+
+#   Recipe.create(
+#     name: repo["title"],
+#     rating: repo["spoonacularScore"],
+#     photo_url: repo["image"],
+#     description: recipe_info["instructions"],
+#     prep_time: recipe_info["preparationMinutes"]
+#   )
